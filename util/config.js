15fe48292b5d7a89e6da9fbe79b44005f0c6f585
AMRESH <amreshaiio@gmail.com>
2023-01-13 18:40:17 +0530

    'ok'

--- util/config.js
+++ util/config.js
@@ -2,8 +2,6 @@ import { config } from "dotenv";
 config();
 
 export const SECRETS = {
-  mongo_uri: process.env.MONGO_URI,
-
   jwt: process.env.JWT_SECRET,
   jwtExp: "1y",
   node_env: process.env.NODE_ENV,
@@ -20,5 +18,5 @@ export const SECRETS = {
   // aws_accessKey_Id: process.env.AWS_ACCESS_KEY_ID,
   // aws_secret_key_Id: process.env.AWS_SECRET_ACCESS_KEY,
   // region: process.env.AWS_REGION,
-  sendGrid: process.env.SEND_GRID,
+  // sendGrid: process.env.SEND_GRID,
 };
